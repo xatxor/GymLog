aa4d08569755c19bb3f9dadf2225897db2f4012c
vika <kiranixis@gmail.com>
2022-09-03 18:26:34 +0300

    settings delete database func

--- GymLog/GymLog/ViewController/SettingsViewController.swift
+++ GymLog/GymLog/ViewController/SettingsViewController.swift
@@ -19,7 +19,67 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configure()
+        setupTableView()
+        
         view.backgroundColor = .white
         title = "Settings"
+    }
+    
+    private func configure(){
+        
+    }
+    
+    private func deleteDB(){
+        let vc = DeleteConfirmationViewController()
+        vc.completion = { isOkay in
+            if isOkay{
+                CoreDataManager.shared.deleteAll()
+            }
+        }
+        navigationController?.present(vc, animated: true)
+    }
+    
+    private let tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    private func setupTableView(){
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.separatorStyle = .none
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+        ])
+    }
+}
+extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Delete database"
+        cell.textLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        deleteDB()
     }
 }
